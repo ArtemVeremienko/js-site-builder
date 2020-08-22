9e36dac43d784d7b8ad2484b0b0616228f75fe02
Artem <temaspsxxx@gmail.com>
2020-08-22 16:42:21 +0300

    Added model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import './styles/index.css'
+import {model} from './model';
+import './styles/index.css';
+
+model.forEach(block => {
+  let html;
+  if (block.type === 'title') {
+    html = title(block);
+  } else if (block.type === 'text') {
+    html = text(block);
+  } else if (block.type === 'textColumns') {
+    html = textColumns(block);
+  }
+
+  document.querySelector('#site').insertAdjacentHTML('beforeend', html);
+});
+
+function title(block) {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <h1>${block.value}</h1>
+      </div>
+    </div>
+  `
+}
+
+function text(block) {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <p>${block.value}</p>
+      </div>
+    </div>
+  `;
+}
+
+function textColumns(block) {
+  const cols = block.value.map(item => `<div class="col-sm">${item}</div>`)
+  
+  return `
+    <div class="row">
+      ${cols.join('')}
+    </div>
+  `;
+}
---
+++ src/model.js
@@ -0,0 +1,27 @@
+export const model = [
+  {
+    type: 'title',
+    value: 'Test Title',
+    options: {}
+  },
+  {
+    type: 'text',
+    value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    options: {}
+  },
+  {
+    type: 'textColumns',
+    value: [
+      '1 text',
+      '2 text',
+      '3 text',
+      '4 text',
+    ],
+    options: {}
+  },
+  {
+    type: 'title',
+    value: 'Test Title',
+    options: {}
+  },
+];
